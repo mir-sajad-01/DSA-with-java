793925d8bbf30e7c3cbd2a977040453b63307460
mir-sajad-01 <121882143+mir-sajad-01@users.noreply.github.com>
2024-12-21 13:48:10 +0530

    Add files via upload

---
+++ _101_gridways.java
@@ -0,0 +1,17 @@
+public class _101_gridways {
+    public static int gridways( int i , int j , int n  , int m){
+        // base case 
+        if ( i ==n-1 && j == m-1){
+            return 1;   // cndn for last call
+        }else if( i == n || j == n){
+            return 0;
+        }
+        int w1 = gridways( i+1, j, n, m);
+        int w2  = gridways ( i , j+1, n ,m);
+        return w1+w2;
+    }
+    public static void main(String[] args) {
+        int n = 3 , m = 3;
+        System.out.println(gridways (0,0,n,m));
+    }
+}
