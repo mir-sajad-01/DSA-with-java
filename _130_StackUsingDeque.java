c541357aeaa16cc868bd62259fe84c60567c6078
mir-sajad-01 <121882143+mir-sajad-01@users.noreply.github.com>
2025-03-15 14:23:10 +0530

    Add files via upload

---
+++ _130_StackUsingDeque.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class _130_StackUsingDeque {
+    public static class Stack{
+        Deque<Integer> d = new LinkedList<>();
+        public void push(int data){
+             d.addLast(data);
+        }
+        public int pop(){
+            return d.removeLast();
+        }
+        public  int peek(){
+            return  d.getLast();
+        }
+    }
+    public static void main(String[] args) {
+        Stack s= new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+    }
+}
