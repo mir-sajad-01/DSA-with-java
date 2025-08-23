7f78fc408e293313adf514bbf3621ffcbf8b16f5
mir-sajad-01 <121882143+mir-sajad-01@users.noreply.github.com>
2025-08-23 13:18:19 +0530

    Add files via upload

---
+++ _151_BinarySearchTree.java
@@ -0,0 +1,40 @@
+public class _151_BinarySearchTree {
+    public static class node{
+        int data;
+        node left;
+        node right;
+        node(int data){
+            this.data = data;
+        }
+    }
+    public static node insert(node root, int val){
+        if(root == null){
+            root = new node(val);
+            return root;
+        }
+        if(root.data< val){
+            root.right = insert(root.right, val);
+        }else{
+            root.left = insert(root.left,val);
+        }
+        return root;
+    }
+    public static void inorder(node root){
+        if(root == null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data +" ");
+        inorder(root.right);
+
+    }
+    public static void main(String[] args) {
+        int values[] = {5,1,3,4,2,7};
+        node root = null;
+        for(int i = 0 ;i<values.length;i++){
+            root = insert(root,values[i]);
+        }
+        inorder(root);
+        System.out.println();
+    }
+}
