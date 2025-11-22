e75ba58163b77d5def9cb1a7667d8aa805d428ac
mir-sajad-01 <121882143+mir-sajad-01@users.noreply.github.com>
2025-11-22 14:10:23 +0530

    Add files via upload

---
+++ _172_LinkedHashMap.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class _172_LinkedHashMap {
+    public static void main(String[] args) {
+        LinkedHashMap<String, Integer> lhm = new LinkedHashMap<>();
+        lhm.put("India",50);
+        lhm.put("US", 67);
+        lhm.put("China",57);
+        System.out.println(lhm);
+    }
+}
